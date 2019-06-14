658e7cc51b8966a5904816bcd17c40464a76b868
Sindhumeenakshi <51654472+Sindhumeenakshi@users.noreply.github.com>
2019-06-14 16:51:48 +0530

    Update subset.py

--- codekatta/subset.py
+++ codekatta/subset.py
@@ -2,6 +2,6 @@ n,m= map(int,input().split())
 set1={int(x) for x in input().split()}
 set2={int(y) for y in input().split()}
 if((set2.issubset(set1))):
-  print("yes")
+  print("YES")
 else:
-  print("no")
+  print("NO")
