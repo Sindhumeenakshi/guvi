03f394dd8752bdf1465e3a537e096b678fc48537
Sindhumeenakshi <51654472+Sindhumeenakshi@users.noreply.github.com>
2019-06-14 16:29:36 +0530

    Update betweenprime.py

--- codekatta/betweenprime.py
+++ codekatta/betweenprime.py
@@ -1,13 +1,15 @@
-t,l=input().split()
-t=int(t)
-l=int(l)
-for i in range(t+1,l):
+m,n=input().split()
+m=int(m)
+n=int(n)
+c=0
+for i in range(m,n):
   if(i>1):
     for j in range(2,i):
       if(i%j==0):
         break
 
     else:
-      print(i,end=' ')
+      c=c+1
   else:
     break
+print(c)
